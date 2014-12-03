beed098bb82d29f716ae94a1227cf2f0cb597831
Omar Rwemi <omsharp@gmail.com>
2014-12-03 21:16:29 +0200

    Initial

---
+++ GameOfLifeTests/CellTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using GameOfLife;
+
+namespace GameOfLifeTests
+{
+    [TestFixture]
+    public class CellTests
+    {
+        [Test]
+        public void CreateNewCell()
+        {
+            var cell = new Cell();
+        }
+    }
+}
